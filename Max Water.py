e9bb30e1fd20b0404e2f898061660fd2693bb669
abhi-bohra <85584406+abhi-bohra@users.noreply.github.com>
2021-06-19 21:35:08 +0530

    Update Max Water.py

--- Max Water.py
+++ Max Water.py
@@ -8,7 +8,7 @@ def water(a):
     area = 0
     for i in range(len(a)):
         for j in range(i+1, len(a)):
-            area = max(area, min(a[i], a[j]) * (j - 1))
+            area = max(area, min(a[i], a[j]) * (j - i))
     return area
 a = list(map(int, input("Enter numbers").strip().split()))
 
